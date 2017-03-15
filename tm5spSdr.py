6ab2a2171235636e760a25d6c3c1c51f58c35e55
Containerhouse <sales@containerhouse.com>
2017-03-15 16:07:22 -0500

    Update tm5spSdr.py

--- tm5spSdr.py
+++ tm5spSdr.py
@@ -1,3 +1,29 @@
+# ----------------------------------------------------------------------
+# Numenta Platform for Intelligent Computing (NuPIC)
+# Copyright (C) 2015-2016, Numenta, Inc.  Unless you have an agreement
+# with Numenta, Inc., for a separate license for this software code, the
+# following terms and conditions apply:
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero Public License for more details.
+#
+# You should have received a copy of the GNU Affero Public License
+# along with this program.  If not, see http://www.gnu.org/licenses.
+#
+# http://numenta.org/licenses/
+# ----------------------------------------------------------------------
+
+#We have adapted network_api_demo.py for our own research.
+#We are only using the SensorRegion from the Network API. Encoded data is being fed to the latest
+#spatial pooler, temporal memory, and SDR classifier algorithms. 
+#The sample data is our own and is generated from our SketchUp ruby extension.
+
 import copy
 import csv
 import json
